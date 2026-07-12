01be737d23df429bd851f1c87e25e21c8faeda54
AndrewI26 <105824138+AndrewI26@users.noreply.github.com>
2026-07-12 18:29:28 -0400

    Add: minimum-stack - submission-10

---
+++ Data Structures & Algorithms/minimum-stack/submission-10.py
@@ -0,0 +1,33 @@
+class MinStack:
+
+    # 1 1 -1
+    def __init__(self):
+        self.stack = []
+        self.min = None
+
+    def push(self, val: int) -> None:
+        if len(self.stack) == 0:
+            self.stack = [0]
+            self.min = val
+        else:
+            diff = val - self.min
+            if diff < 0:
+                self.min = val
+            self.stack += [diff]
+            
+
+
+    def pop(self) -> None:
+        diff  = self.stack.pop()
+        if diff < 0:
+            self.min -= diff
+
+    def top(self) -> int:
+        if self.stack[-1] < 0:
+            return self.min
+        else:
+            return self.min + self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min
+        
