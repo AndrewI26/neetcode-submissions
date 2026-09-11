e0fd2cbbdcd177c298ba5ac2543fe3df55c21501
AndrewI26 <105824138+AndrewI26@users.noreply.github.com>
2026-09-11 19:09:30 -0400

    Add: pacific-atlantic-water-flow - submission-0

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.py
@@ -0,0 +1,59 @@
+'''
+res = []
+cache = 2d arr r x c
+
+for r in range():
+    for l in range()
+'''
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        res = []
+        cache = {} # { (r, c): (reaches_atlantic: bool, reaches_pacfici: bool) }
+
+        for c in range(len(heights[0])):
+            for r in range(len(heights)):
+                def dfs(r, c) -> (bool, bool):
+                    reaches_atlantic = False
+                    reaches_pacific = False
+
+                    if (r, c) in cache:
+                        return cache[(r, c)]
+
+                    # up check
+                    if r < 0: 
+                        return (False, True)
+                    # down check
+                    if r >= len(heights): 
+                        return (True, False)
+                    # right check
+                    if c >= len(heights[0]): 
+                        return (True, False)
+                    # left check
+                    if c < 0: 
+                        return (False, True)
+
+                    deltas = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+                    for (r_delta, c_delta) in deltas:
+                        new_r, new_c = r + r_delta, c + c_delta
+                        if 0 <= new_r < len(heights) and 0 <= new_c < len(heights[0]) and not heights[new_r][new_c] < heights[r][c]:
+                            continue
+                        
+                        (reaches_a, reaches_p) = dfs(new_r, new_c)
+                        if reaches_a:
+                            reaches_atlantic = True
+                        if reaches_p:
+                            reaches_pacific = True
+                    result = (reaches_atlantic, reaches_pacific)
+                    cache[(r, c)] = result
+
+                    if reaches_atlantic and reaches_pacific:
+                        res.append([r ,c])
+                    return result
+                
+                dfs(r, c)
+        
+        return res
+
+
+
+
