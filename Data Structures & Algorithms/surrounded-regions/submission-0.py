42a5557a450d1301215d985f74d83ab3ffe6e302
AndrewI26 <105824138+AndrewI26@users.noreply.github.com>
2026-09-18 09:58:13 -0400

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        width = len(board[0])
+        height = len(board)
+
+        connected_to_edge = set() #(col, row)
+
+        q = deque()
+        for row in range(height):
+            q.append((0, row))
+            connected_to_edge.add((0, row))
+
+            q.append((width - 1, row))
+            connected_to_edge.add((width - 1, row))
+
+        while q:
+            (col, row) = q.popleft()
+
+            if (col, row) in connected_to_edge:
+                continue
+            if col < 0 or col >= width:
+                continue
+            if row < 0 or row >= height:
+                continue
+            
+            for dx, dy in directions:
+                new_col = col + dx
+                new_row = row + dy
+
+                q.append((new_col, new_row))
+                connected_to_edge.add((new_col, new_row))
+
+        
+        for row in range(height):
+            for col in range(width):
+                if board[row][col] == "O" and (row, col) not in connected_to_edge:
+                    board[row][col] = "X"
+
+
+            
+            
+
+            
+
